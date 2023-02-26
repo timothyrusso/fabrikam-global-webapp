d2fd282a625970806e873d879355f662a51b7d93
timothyrusso <russotimothy@live.it>
2023-02-26 15:27:58 +0100

    feat: change port number

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use('/users', require('./users/user.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => console.log('Server listening on port: ' + port));
