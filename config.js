46f8e6c519b16a7988ec789b9da8fadc5936d5e7
timothyrusso <russotimothy@live.it>
2023-02-26 10:18:04 +0100

    feat: add config object

---
+++ config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  dbName: 'fabrikam',
+  dbConfig: {
+    server: 'fabrikam-server.database.windows.net',
+    options: {
+      port: 1433,
+      trustServerCertificate: true,
+      encrypt: true,
+    },
+    authentication: {
+      type: 'default',
+      options: {
+        userName: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      },
+    },
+  },
+};
--- helpers/db.js
+++ helpers/db.js
@@ -1,51 +1,55 @@
 const tedious = require('tedious');
 const { Sequelize } = require('sequelize');
 
-const { dbName, dbConfig } = require('config.json');
+const config = require('config.js');
+const { dbName, dbConfig } = config;
 
 module.exports = db = {};
 
 initialize();
 
 async function initialize() {
-    const dialect = 'mssql';
-    const host = dbConfig.server;
-    const { userName, password } = dbConfig.authentication.options;
+  const dialect = 'mssql';
+  const host = dbConfig.server;
+  const { userName, password } = dbConfig.authentication.options;
 
-    // create db if it doesn't already exist
-    await ensureDbExists(dbName);
+  // create db if it doesn't already exist
+  await ensureDbExists(dbName);
 
-    // connect to db
-    const sequelize = new Sequelize(dbName, userName, password, { host, dialect });
+  // connect to db
+  const sequelize = new Sequelize(dbName, userName, password, {
+    host,
+    dialect,
+  });
 
-    // init models and add them to the exported db object
-    db.User = require('../users/user.model')(sequelize);
+  // init models and add them to the exported db object
+  db.User = require('../users/user.model')(sequelize);
 
-    // sync all models with database
-    await sequelize.sync({ alter: true });
+  // sync all models with database
+  await sequelize.sync({ alter: true });
 }
 
 async function ensureDbExists(dbName) {
-    return new Promise((resolve, reject) => {
-        const connection = new tedious.Connection(dbConfig);
-        connection.connect((err) => {
-            if (err) {
-                console.error(err);
-                reject(`Connection Failed: ${err.message}`);
-            }
+  return new Promise((resolve, reject) => {
+    const connection = new tedious.Connection(dbConfig);
+    connection.connect((err) => {
+      if (err) {
+        console.error(err);
+        reject(`Connection Failed: ${err.message}`);
+      }
 
-            const createDbQuery = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
-            const request = new tedious.Request(createDbQuery, (err) => {
-                if (err) {
-                    console.error(err);
-                    reject(`Create DB Query Failed: ${err.message}`);
-                }
+      const createDbQuery = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
+      const request = new tedious.Request(createDbQuery, (err) => {
+        if (err) {
+          console.error(err);
+          reject(`Create DB Query Failed: ${err.message}`);
+        }
 
-                // query executed successfully
-                resolve();
-            });
+        // query executed successfully
+        resolve();
+      });
 
-            connection.execSql(request);
-        });
+      connection.execSql(request);
     });
+  });
 }
